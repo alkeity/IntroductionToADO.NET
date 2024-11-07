54aac6e51dd5d2e15748ba8d4d5dd489287cd9e9
alkeity <alkeity.23@gmail.com>
2024-11-07 16:02:01 +0300

    homework: replace textbox with combobox with query options

---
+++ Library2/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Library2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnExecute = new System.Windows.Forms.Button();
+			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.tbQuery = new System.Windows.Forms.ComboBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnExecute
+			// 
+			this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnExecute.Location = new System.Drawing.Point(416, 13);
+			this.btnExecute.Name = "btnExecute";
+			this.btnExecute.Size = new System.Drawing.Size(75, 21);
+			this.btnExecute.TabIndex = 1;
+			this.btnExecute.Text = "Execute";
+			this.btnExecute.UseVisualStyleBackColor = true;
+			this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+			// 
+			// dataGridView
+			// 
+			this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView.Location = new System.Drawing.Point(12, 40);
+			this.dataGridView.Name = "dataGridView";
+			this.dataGridView.Size = new System.Drawing.Size(479, 349);
+			this.dataGridView.TabIndex = 2;
+			// 
+			// tbQuery
+			// 
+			this.tbQuery.FormattingEnabled = true;
+			this.tbQuery.Items.AddRange(new object[] {
+            "SELECT * FROM Authors",
+            "SELECT * FROM Books",
+            "SELECT * FROM Genres"});
+			this.tbQuery.Location = new System.Drawing.Point(12, 13);
+			this.tbQuery.Name = "tbQuery";
+			this.tbQuery.Size = new System.Drawing.Size(383, 21);
+			this.tbQuery.TabIndex = 3;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(503, 401);
+			this.Controls.Add(this.tbQuery);
+			this.Controls.Add(this.dataGridView);
+			this.Controls.Add(this.btnExecute);
+			this.Name = "MainForm";
+			this.Text = "Library";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button btnExecute;
+		private System.Windows.Forms.DataGridView dataGridView;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.ComboBox tbQuery;
+	}
+}
+
---
+++ Library2/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Library2
+{
+	public partial class MainForm : Form
+	{
+		readonly string connectionString;
+		SqlConnection connection;
+
+		public MainForm()
+		{
+			connectionString = ConfigurationManager.ConnectionStrings["LibraryConnectionString"].ConnectionString;
+			connection = new SqlConnection(connectionString);
+			InitializeComponent();
+		}
+
+		private void btnExecute_Click(object sender, EventArgs e)
+		{
+			string cmdString = tbQuery.Text;
+			SqlCommand cmd = new SqlCommand(cmdString, connection);
+			connection.Open();
+			DataTable dt = new DataTable();
+			SqlDataReader dr = cmd.ExecuteReader();
+			for (int i = 0; i < dr.FieldCount; i++) dt.Columns.Add(dr.GetName(i));
+
+			while (dr.Read())
+			{
+				DataRow row = dt.NewRow();
+				for (int i = 0; i < dr.FieldCount; i++) row[i] = dr[i];
+				dt.Rows.Add(row);
+			}
+			dr.Close();
+			connection.Close();
+			dataGridView.DataSource = dt;
+		}
+	}
+}
