f1d2bc7f165ec99f761869edccd195e4dcacdd7c
alkeity <alkeity.23@gmail.com>
2024-11-27 15:52:15 +0300

    feat: filter groups by study fields WIP

--- AcademyDataSet/MainForm.Designer.cs
+++ AcademyDataSet/MainForm.Designer.cs
@@ -28,21 +28,72 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.labelStudyField = new System.Windows.Forms.Label();
+			this.labelGroup = new System.Windows.Forms.Label();
+			this.comboFields = new System.Windows.Forms.ComboBox();
+			this.comboGroups = new System.Windows.Forms.ComboBox();
 			this.SuspendLayout();
+			// 
+			// labelStudyField
+			// 
+			this.labelStudyField.AutoSize = true;
+			this.labelStudyField.Location = new System.Drawing.Point(18, 19);
+			this.labelStudyField.Name = "labelStudyField";
+			this.labelStudyField.Size = new System.Drawing.Size(62, 13);
+			this.labelStudyField.TabIndex = 0;
+			this.labelStudyField.Text = "Study Field:";
+			// 
+			// labelGroup
+			// 
+			this.labelGroup.AutoSize = true;
+			this.labelGroup.Location = new System.Drawing.Point(38, 55);
+			this.labelGroup.Name = "labelGroup";
+			this.labelGroup.Size = new System.Drawing.Size(42, 13);
+			this.labelGroup.TabIndex = 1;
+			this.labelGroup.Text = "Group: ";
+			// 
+			// comboFields
+			// 
+			this.comboFields.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboFields.FormattingEnabled = true;
+			this.comboFields.Location = new System.Drawing.Point(85, 15);
+			this.comboFields.Name = "comboFields";
+			this.comboFields.Size = new System.Drawing.Size(192, 21);
+			this.comboFields.TabIndex = 2;
+			this.comboFields.SelectedIndexChanged += new System.EventHandler(this.comboFields_SelectedIndexChanged);
+			// 
+			// comboGroups
+			// 
+			this.comboGroups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboGroups.FormattingEnabled = true;
+			this.comboGroups.Location = new System.Drawing.Point(85, 51);
+			this.comboGroups.Name = "comboGroups";
+			this.comboGroups.Size = new System.Drawing.Size(192, 21);
+			this.comboGroups.TabIndex = 3;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(305, 112);
+			this.Controls.Add(this.comboGroups);
+			this.Controls.Add(this.comboFields);
+			this.Controls.Add(this.labelGroup);
+			this.Controls.Add(this.labelStudyField);
 			this.Name = "MainForm";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.MainForm_Load);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label labelStudyField;
+		private System.Windows.Forms.Label labelGroup;
+		private System.Windows.Forms.ComboBox comboFields;
+		private System.Windows.Forms.ComboBox comboGroups;
 	}
 }
 
--- AcademyDataSet/MainForm.cs
+++ AcademyDataSet/MainForm.cs
@@ -72,20 +72,42 @@ namespace AcademyDataSet
 			}
 			finally { connection.Close(); }
 
-			foreach (DataRow row in groupsRelatedData.Tables[dsTableFields].Rows)
-			{
-				Console.WriteLine($"{row["id"]}: {row["study_field_name"]}");
-			}
+			//foreach (DataRow row in groupsRelatedData.Tables[dsTableFields].Rows)
+			//{
+			//	Console.WriteLine($"{row["id"]}: {row["study_field_name"]}");
+			//}
 		}
 
 		private void MainForm_Load(object sender, EventArgs e)
 		{
 			InitDataSet();
+			comboFields.DataSource = groupsRelatedData.Tables["StudyFields"];
+			comboFields.DisplayMember = "study_field_name";
+			comboFields.ValueMember = "id";
+			comboFields.SelectedIndex = 0;
+
+			comboGroups.DataSource = groupsRelatedData.Tables["Groups"];
+			comboGroups.DisplayMember = "group_name";
+			comboGroups.ValueMember = "id";
+			comboGroups.SelectedIndex = 0;
+		}
+
+		private void comboFields_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			object selVal = comboFields.SelectedValue;
+			//Console.WriteLine(filter);
+			if (selVal.ToString() != selVal.GetType().ToString())
+			{
+				string filter = $"study_field_id = {comboFields.SelectedValue}";
+				groupsRelatedData.Tables["Groups"].DefaultView.RowFilter = filter;
+			}
 		}
 
 		[DllImport("kernel32.dll")]
 		private static extern bool AllocConsole();
 		[DllImport("kernel32.dll")]
 		private static extern bool FreeConsole();
+
+		
 	}
 }
